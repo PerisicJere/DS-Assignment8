265423101213c131de0c50d93eb1f90e0014360c
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2023-11-15 21:57:20 -0500

    Trying to make methods 20 lines of code

---
+++ src/program8.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class program8 {
+    public static void main(String[] args) {
+        Map<String, myAVL> carMakeTrees = new TreeMap<>();
+        String file = args[0];
+        String testFile = args[1];
+        String resultFile = "result.csv";
+        insertFromCSV(file, carMakeTrees);
+        processTestFile(testFile, carMakeTrees, resultFile);
+
+
+    }
+    private static SaleRecord parseCSVLine(String line) {
+        String[] parts = line.split(",");
+        if (parts.length != 9) {
+            return null;
+        }
+
+        String dateStr = parts[0];
+        String salesperson = parts[1];
+        String customerName = parts[2];
+        String carMake = parts[3];
+        String carModel = parts[4];
+        int carYear = Integer.parseInt(parts[5]);
+        double salePrice = Double.parseDouble(parts[6]);
+        double commissionRate = Double.parseDouble(parts[7]);
+        double commissionEarned = Double.parseDouble(parts[8]);
+
+        return new SaleRecord(dateStr, salesperson, customerName, carMake, carModel, carYear, salePrice, commissionRate, commissionEarned);
+    }
+
+    private static void insertFromCSV(String csvFilePath, Map<String, myAVL> carMakeTrees) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(csvFilePath))) {
+            String line;
+            boolean skipHeader = true;
+            while ((line = reader.readLine()) != null) {
+                if (skipHeader) {
+                    skipHeader = false;
+                    continue;
+                }
+                SaleRecord saleRecord = parseCSVLine(line);
+                if (saleRecord != null) {
+                    String carMake = saleRecord.getCarMake();
+                    if (!carMakeTrees.containsKey(carMake)) {
+                        carMakeTrees.put(carMake, new myAVL());
+                    }
+                    myAVL saleRecordTree = carMakeTrees.get(carMake);
+                    saleRecordTree.add(saleRecord);}
+            }
+        } catch (IOException e) {
+            e.printStackTrace();}
+    }
+    private static void processTestFile(String testFile, Map<String, myAVL> carMakeTrees, String resultFile) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(testFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length != 2) {
+                    continue;
+                }
+                String carMake = parts[0].trim();
+                String date = parts[1].trim();
+                myAVL saleRecordTree = carMakeTrees.get(carMake);
+                if (saleRecordTree != null) {
+                    List<SaleRecord> saleRecords = saleRecordTree.search(date);
+                    writer.write(saleRecords.size() + "\n");
+                }else{
+                    writer.write(0 + "\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();}
+    }
+}
